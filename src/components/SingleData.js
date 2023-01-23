32513ab537225199e9c7124f4636690bcf85c255
hasanrc24 <hasanrc24@gmail.com>
2023-01-23 19:58:52 +0600

    abc

--- src/components/SingleData.js
+++ src/components/SingleData.js
@@ -10,20 +10,34 @@ const SingleData = () => {
   };
   // console.log(answer);
   return (
-    <div className="flex gap-4">
-      <div className="flex items-center">{data[currentQuestion]?.question}</div>
+    <div className={currentQuestion < data.length ? `flex gap-4` : "flex"}>
+      <div className="flex flex-col items-center justify-center">
+        <p>{data[currentQuestion]?.question}</p>
+        {currentQuestion < data.length && (
+          <p>{currentQuestion + 1 + "/" + data.length}</p>
+        )}
+      </div>
       <div className="">
-        {data[currentQuestion]?.options?.map((option, i) => {
-          return (
-            <button
-              className="block bg-slate-600 hover:bg-slate-800 my-3 p-2 w-full rounded-md"
-              key={i}
-              onClick={handleAnswer}
-            >
-              {option}
-            </button>
-          );
-        })}
+        {currentQuestion >= data.length ? (
+          <button
+            onClick={() => setCurrentQuestion(0)}
+            className="bg-slate-600 hover:bg-slate-800 my-3 p-2 rounded-md"
+          >
+            Go back
+          </button>
+        ) : (
+          data[currentQuestion]?.options?.map((option, i) => {
+            return (
+              <button
+                className="block bg-slate-600 hover:bg-slate-800 my-3 p-2 w-full rounded-md"
+                key={i}
+                onClick={handleAnswer}
+              >
+                {option}
+              </button>
+            );
+          })
+        )}
       </div>
     </div>
   );
