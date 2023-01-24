9245fd52fef3675742606abb4fa2f3adfa1b5bff
hasanrc24 <hasanrc24@gmail.com>
2023-01-24 23:53:11 +0600

    Mod

--- src/components/SingleData.js
+++ src/components/SingleData.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { data } from "../dummyData";
+
 const SingleData = () => {
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answer, setAnswer] = useState([]);
@@ -10,34 +11,37 @@ const SingleData = () => {
   };
   // console.log(answer);
   return (
-    <div className={currentQuestion < data.length ? `flex gap-4` : "flex"}>
-      <div className="flex flex-col items-center justify-center">
-        <p>{data[currentQuestion]?.question}</p>
-        {currentQuestion < data.length && (
-          <p>{currentQuestion + 1 + "/" + data.length}</p>
-        )}
-      </div>
-      <div className="">
-        {currentQuestion >= data.length ? (
-          <button
-            onClick={() => setCurrentQuestion(0)}
-            className="bg-slate-600 hover:bg-slate-800 my-3 p-2 rounded-md"
-          >
-            Go back
-          </button>
-        ) : (
-          data[currentQuestion]?.options?.map((option, i) => {
-            return (
-              <button
-                className="block bg-slate-600 hover:bg-slate-800 my-3 p-2 w-full rounded-md"
-                key={i}
-                onClick={handleAnswer}
-              >
-                {option}
-              </button>
-            );
-          })
-        )}
+    <div>
+      <p className="text-center mx-auto">Simple React Quiz</p>
+      <div className={currentQuestion < data.length ? `flex gap-4` : "flex"}>
+        <div className="flex flex-col items-center justify-center">
+          <p>{data[currentQuestion]?.question}</p>
+          {currentQuestion < data.length && (
+            <p>{currentQuestion + 1 + "/" + data.length}</p>
+          )}
+        </div>
+        <div className="">
+          {currentQuestion >= data.length ? (
+            <button
+              onClick={() => setCurrentQuestion(0)}
+              className="bg-slate-600 hover:bg-slate-800 my-3 p-2 rounded-md"
+            >
+              Go back
+            </button>
+          ) : (
+            data[currentQuestion]?.options?.map((option, i) => {
+              return (
+                <button
+                  className="block bg-slate-600 hover:bg-slate-800 my-3 p-2 w-full rounded-md"
+                  key={i}
+                  onClick={handleAnswer}
+                >
+                  {option}
+                </button>
+              );
+            })
+          )}
+        </div>
       </div>
     </div>
   );
